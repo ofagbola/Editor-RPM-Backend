8212c1c2c7361ea81d82cfeabd439cd096761119
Obi Christopher <ikchristo19@gmail.com>
2024-02-27 01:32:10 +0000

    jsoijsf

--- src/interfaces/questionnaire.interface.ts
+++ src/interfaces/questionnaire.interface.ts
@@ -1,3 +1,25 @@
+type questionType = "singleChoice" | "multipleChoice" | "numberScale" | "longAnswer" | "shortAnswer" | "existngQuestion";
+
 export interface OuestionnaireRequest {
-    phone_number: string;
+    id: string;
+    question: string;
+    name: string;
+    description: string;
+    answers: string[] | number[];
+    questionCode: string;
+    questionType: questionType;
+    status: string;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export interface AnswerRequest {
+    id: string;
+    questionId: string;
+    answers: string[] | number[];
+    answerType: questionType;
+    userId: string;
+    status: string;
+    created_at: Date;
+    updated_at: Date;
 }
