d997f5167771f9685d1688d300776e95c958189b
NwaforAugustine321 <nwaforaugustine422@gmail.com>
2024-02-28 15:55:31 +0100

    Mobile ui folder structure

---
+++ frontend/App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='flex-1 items-center justify-center bg-red'>
+      <Text className='text-red'>
+        Open up App.js to start working on your app!
+      </Text>
+    </View>
+  );
+}
+
+
